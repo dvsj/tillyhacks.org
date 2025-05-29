551f00aafedde3be6d416dbccb56a89f723f7b18
dev suraj <68204629+aqxq@users.noreply.github.com>
2025-05-29 02:34:59 +0000

    the config rewrote itself when i wasn't watching

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        'studious-xylophone-vwr5xq47746c47v-3000.app.github.dev/',
-        'localhost:3000'
-        // Add any other domains you might use
-      ],
-    },
-  },
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
